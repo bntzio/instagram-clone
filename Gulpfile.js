db64de5d8c6cc12f4d28899d74108b2caa6eee10
Enrique Benitez <enriquebenitezjr@gmail.com>
2016-05-19 00:25:44 -0500

    Add more automation with npm scripts

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,6 +4,7 @@ const rename = require('gulp-rename');
 const babel = require('babelify');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
+const watchify = require('watchify');
 
 gulp.task('styles', () => {
   gulp
@@ -19,13 +20,34 @@ gulp.task('assets', () => {
     .pipe(gulp.dest('public'));
 });
 
-gulp.task('scripts', () => {
-  browserify('./src/index.js')
-    .transform(babel)
-    .bundle()
-    .pipe(source('index.js'))
-    .pipe(rename('app.js'))
-    .pipe(gulp.dest('public'));
+function compile(watch) {
+  const bundle = watchify(browserify('./src/index.js'));
+
+  function rebundle() {
+    bundle
+      .transform(babel)
+      .bundle()
+      .pipe(source('index.js'))
+      .pipe(rename('app.js'))
+      .pipe(gulp.dest('public'));
+  }
+
+  if (watch) {
+    bundle.on('update', () => {
+      console.log('--> Bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+gulp.task('build', function () {
+  return compile();
 });
 
-gulp.task('default', ['styles', 'assets', 'scripts']);
+gulp.task('watch', function () {
+  return compile(true);
+});
+
+gulp.task('default', ['styles', 'assets', 'build']);
